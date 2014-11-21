49d0d34521948b2141255993c49fb0fb4f6d94f4
Ely De La Cruz <elycruz@elycruz.com>
2014-11-20 23:04:12 -0500

    Concat task progress.

---
+++ frontend-dev/pre-artifacts/js/example-feature-1.js
@@ -0,0 +1,20 @@
+/*!
+ * Company Name http://www.company-website.com
+ * .js 
+ * {{hash goes here}}
+ */
+/**
+ * Created by edelacruz on 11/19/2014.
+ */
+/**
+ * Created by edelacruz on 11/19/2014.
+ */
+$(function () {
+    $('body').append('<h1>Hi, this is example-feature-1\'s script result.</h1>');
+});
+/**
+ * Created by edelacruz on 11/19/2014.
+ */
+$(function () {
+    $("body").append('<h4>"example-feature-1/subcomponent1.js" just ran.</h4>');
+});
---
+++ frontend-dev/pre-artifacts/js/example-feature-2.js
@@ -0,0 +1,11 @@
+/*!
+ * Company Name http://www.company-website.com
+ * .js 
+ * {{hash goes here}}
+ */
+/**
+ * Created by edelacruz on 11/19/2014.
+ */
+$(function () {
+   $('body').append('<h2>Hi, this is example-feature-2\'s script result.</h2>');
+});
