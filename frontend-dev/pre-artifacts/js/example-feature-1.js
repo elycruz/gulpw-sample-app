2d0529cfe49b04fad887b7429ea88fc69edf4e3d
Ely De La Cruz <elycruz@elycruz.com>
2014-11-22 17:56:52 -0500

    Rebuilt all build files with the upgraded  module with updated minify and concat tasks.

---
+++ frontend-dev/pre-artifacts/js/example-feature-1.js
@@ -0,0 +1,20 @@
+/*!
+ * Company Name http://www.company-website.com
+ * example-feature-1.js 0.0.0
+ * {{file hash here}}
+ */
+/**
+ * Created by edelacruz on 11/19/2014.
+ */
+/**
+ * Created by edelacruz on 11/19/2014.
+ */
+$(function () {
+    $('body').append('<h1>Hi, this is example-feature-1\'s script result.</h1>');
+});
+/**
+ * Created by edelacruz on 11/19/2014.
+ */
+$(function () {
+    $("body").append('<h4>"example-feature-1/subcomponent1.js" just ran.</h4>');
+});
---
+++ public/js/example-feature-2.js
@@ -0,0 +1,2 @@
+/*! Company Name http://www.company-website.com example-feature-2.js 0.0.0 {{file hash here}} */
+$(function(){$("body").append("<h2>Hi, this is example-feature-2's script result.</h2>")});
